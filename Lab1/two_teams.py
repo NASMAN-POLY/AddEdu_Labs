437a4c5a2c91c60a9a0614ba1ca53c36da39893c
NASMAN-POLY <anastasia182999@gmail.com>
2024-11-04 15:44:23 +0300

    Delete Lab1 directory

--- Lab1/two_teams.py
+++
@@ -1,8 +0,0 @@
-list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
-
-len_list = len(list_players)
-middle_index = len_list // 2
-list_players1 = list_players[:middle_index]
-list_players2 = list_players[middle_index:]
-print(list_players1)
-print(list_players2)
